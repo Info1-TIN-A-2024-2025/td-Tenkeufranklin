63b668de380a1f8795095bf19492f28ecd229774
Tenkeufranklin <franklin.tenkeuazegha@heig-vd.ch>
2024-10-18 12:54:16 +0000

    TD 2024_10_18

--- TD_20240920/td.c
+++ TD_20240920/td.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // inclusion du ficjier stdio.h
 
-int main(int argc, const char *argv[])
-{
-	puts("Good morning, you can begin working now...!");	
-	return 0;
-}
+int main(int argc, const char *argv[])					 // point d'entrée du programme
+{														 // début du bloc main
+	puts("Good morning, you can begin working now...!"); // instruction d'affichage
+	return 0;											 // instruction de fin de programme
+} // fin du bloc main
